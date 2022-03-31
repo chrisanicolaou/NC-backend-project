feef5b1be584f08b0caf6ea61d568b8989067c7a
Christos Nicolaou <chrisanicolaou>
2022-03-31 10:10:36 +0100

    Creates production database using Heroku

--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,18 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE not set");
 }
+
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
 
 module.exports = new Pool();
